ba8e4ce9d14ed39a7b70f9aaf7fbe3f12f0ee7ee
Sheysonn44 <Sheysonn44@gmail.com>
2025-07-03 21:51:01 -0600

    Repo

--- api/index.js
+++ api/index.js
@@ -9,19 +9,19 @@ const login = require("../src/routes/loginrouters")
 const teacher = require("../src/routes/teacher.routes")
 
 //const allowedOrigins = ['http://localhost:5173', 'http://localhost:5175'];
-const allowedOrigins = ['https://front-umber-sigma.vercel.app/', 'http://localhost:5173', ];
+//const allowedOrigins = ['https://front-umber-sigma.vercel.app/', 'http://localhost:5173', ];
 
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('No permitido por CORS'));
-    }
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+//   origin: function(origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('No permitido por CORS'));
+//     }
+//   },
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 // Ruta base para utilizar el servicio
